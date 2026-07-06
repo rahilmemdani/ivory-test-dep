5e27d45bccda958a1270b079525252fbda7d02b0
rahilmemdani <websiteadmin-5707>
2026-07-06 23:10:19 +0930

    Attempt base portal-embed URL

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -10,7 +10,7 @@ import { Plume } from "@/components/site/Plume";
  */
 const BOOKING_URL =
   (process.env.NEXT_PUBLIC_BOOKING_URL as string | undefined) ??
-  "https://ivoryatelier.zohobookings.com.au/portal-embed#/35653000000044005";
+  "https://ivoryatelier.zohobookings.com.au/portal-embed";
 
 export default function Book() {
   return (
